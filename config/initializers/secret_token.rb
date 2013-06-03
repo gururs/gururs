38304f9de2b556bfd3dfb739bc774f2a4026c11d
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-06-02 21:14:28 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gururs::Application.config.secret_key_base = '5234544a1b878ea1dbb26110efbff2658b63dcb8b4929630ce47dfe8fd6df0cb86f470175b3bb78b8fca76363a64c3689a96494aade9ecfa2abb05248e01dcff'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gururs::Application.config.session_store :cookie_store, key: '_gururs_session'
