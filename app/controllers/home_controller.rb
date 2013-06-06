5e9470207af65ada9937f60697aed7b869889276
Patrick Edgar Müller <patrick.e.muller@gmail.com>
2013-06-06 00:26:42 -0300

    Created Page About

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,7 @@ class HomeController < ApplicationController
     @events      = Event.next.limit(3)
     @video_talks = Talk.with_video.last(3)
   end
+
+  def about
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Gururs::Application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   root to: 'home#index'
+
+  get '/about' => 'home#about'
 end
