c6ecd9edcb4e4d4989e174d54b5c2f78af0b15d1
Irio Irineu Musskopf Junior <iirineu@gmail.com>
2013-06-05 23:47:05 -0300

    Render YouTube player in Talk partial (closes #2)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def video_thumb_image(entity_with_video)
     image_tag("http://img.youtube.com/vi/#{entity_with_video.video_code}/0.jpg")
   end
+
+  def video_thumb_video(entity_with_video)
+    content_tag('iframe', nil, width:           '280',
+                               height:          '157',
+                               frameborder:     '0',
+                               allowfullscreen: true,
+                               src:             "https://www.youtube.com/embed/#{entity_with_video.video_code}")
+  end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,12 +1,19 @@
 require 'spec_helper'
 
 describe ApplicationHelper do
-  describe "video_thumb_image" do
-    let(:talk) { double(video_code: '8_WHlskbwnc') }
+  let(:talk) { double(video_code: '8_WHlskbwnc') }
 
-    it "returns a image from the YouTube's video based on video_code attribute of the parameter" do
+  describe "video_thumb_image" do
+    it "returns a image from the YouTube's video based on parameters' video_code attribute" do
       expected_html = image_tag('http://img.youtube.com/vi/8_WHlskbwnc/0.jpg')
       expect(helper.video_thumb_image(talk)).to eql(expected_html)
+    end
+  end
+
+  describe "video_thumb_video" do
+    it "returns a video player from YouTube based on parameters' video_code attribute" do
+      expected_html = "<iframe allowfullscreen=\"true\" frameborder=\"0\" height=\"157\" src=\"https://www.youtube.com/embed/8_WHlskbwnc\" width=\"280\"></iframe>"
+      expect(helper.video_thumb_video(talk)).to eql(expected_html)
     end
   end
 end
