139d1ec48c6be37c5ad949092b22a2626c3c65ab
Irio Irineu Musskopf Junior <iirineu@gmail.com>
2013-06-07 20:45:15 -0300

    Order events in EventsController#index by happens_at DESC

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,3 +1,9 @@
 class EventsController < InheritedResources::Base
   actions :index, :show
+
+  protected
+
+  def collection
+    @events ||= end_of_association_chain.order('happens_at DESC')
+  end
 end
