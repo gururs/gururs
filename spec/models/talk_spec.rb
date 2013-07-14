fb53a0dcb4be12b1777972f0f4407a57364bd52b
Miguel Grazziotin <miguelgraz@gmail.com>
2013-07-14 17:34:46 -0300

    showing talks even if it doesnt have any media

--- spec/models/talk_spec.rb
+++ spec/models/talk_spec.rb
@@ -35,6 +35,28 @@ describe Talk do
     end
   end
 
+  describe '#no_media?' do
+    context 'when not audio neither video code is present' do
+      subject { Talk.make! audio_code: nil, video_code: nil }
+      it { expect(subject.no_media?).to be_true }
+    end
+
+    context 'when video code is present' do
+      subject { Talk.make! video_code: 111 }
+      it { expect(subject.no_media?).to be_false }
+    end
+
+    context 'when audio code is present' do
+      subject { Talk.make! audio_code: 222 }
+      it { expect(subject.no_media?).to be_false }
+    end
+
+    context 'when with video and audio code' do
+      subject { Talk.make! video_code: 333, audio_code: 333 }
+      it { expect(subject.no_media?).to be_false }
+    end
+  end
+
   describe 'Scopes' do
     describe '.with_video' do
       before do
@@ -50,6 +72,15 @@ describe Talk do
         Talk.make! audio_code: nil
       end
       it { expect(Talk.with_audio).to have(2).videos }
+    end
+
+    describe '.without_media' do
+      before do
+        3.times { Talk.make! audio_code: nil, video_code: nil }
+        Talk.make! video_code: 111
+        Talk.make! audio_code: 222
+      end
+      it { expect(Talk.without_media).to have(3).videos }
     end
   end
 end
